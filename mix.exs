335a0be79da7e1fcc798813caef8e2c860f47d0a
Frank Hunleth <fhunleth@troodon-software.com>
2017-12-08 20:44:15 -0500

    Enable the linter

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule NervesSystemX8664.Mixfile do
     [
       {:nerves, "~> 0.8", runtime: false},
       {:nerves_system_br, "~> 0.15.1", runtime: false},
-      {:nerves_toolchain_x86_64_unknown_linux_musl, "~> 0.12.1", runtime: false}
+      {:nerves_toolchain_x86_64_unknown_linux_musl, "~> 0.12.1", runtime: false},
+      {:nerves_system_linter, "~> 0.2.2", runtime: false}
     ]
   end
 
