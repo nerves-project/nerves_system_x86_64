872d4024c75c97cb8da78e1008019a33d9c8d8fb
Frank Hunleth <fhunleth@troodon-software.com>
2017-12-15 16:02:29 -0500

    Mount boot partition ro to simplify debug
    Since x86_64 uses /boot to hold the kernel, the boot partition is
    mounted at /boot/grub. This looks a little weird, but for debug
    purposes, it's fine. VFAT support in the kernel needed to be enabled for
    this to work.

--- post-build.sh
+++ post-build.sh
@@ -10,7 +10,7 @@ fi
 # Remove the Buildroot-generated grub.cfg so avoid confusion.
 # We put our grub in the FAT filesystem at the beginning of the
 # disk so that it exists across firmware updates.
-rm -fr $TARGET_DIR/boot/grub
+rm -fr $TARGET_DIR/boot/grub/*
 
 # Run the common post-build processing for nerves
 $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-build.sh $TARGET_DIR
