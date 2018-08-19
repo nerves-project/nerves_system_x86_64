763f48ebe057a56d8ce26bf035dfbf530f510869
Justin Schneck <jschneck@mac.com>
2018-08-18 21:52:49 -0400

    Add provisioning.conf and include it in fwup.conf

--- post-build.sh
+++ post-build.sh
@@ -37,3 +37,6 @@ rm -fr $TARGET_DIR/boot/grub/*
 # active firmware.
 mkdir -p $TARGET_DIR/usr/share/fwup
 NERVES_SYSTEM=$BASE_DIR $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_DIR/usr/share/fwup/revert.fw
+
+# Copy the fwup includes to the images dir
+cp -rf $NERVES_DEFCONFIG_DIR/fwup_include $BINARIES_DIR
