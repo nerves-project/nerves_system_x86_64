692beb69c2e020fa8e1af23ee69bf3ece5e2d336
Frank Hunleth <fhunleth@troodon-software.com>
2019-06-07 10:54:37 -0400

    nerves_system_br: bump to v1.8.0
    This also bumps the toolchain to v1.2.0.

--- mix.exs
+++ mix.exs
@@ -48,9 +48,9 @@ defmodule NervesSystemX8664.MixProject do
 
   defp deps do
     [
-      {:nerves, "~> 1.3", runtime: false},
-      {:nerves_system_br, "1.7.2", runtime: false},
-      {:nerves_toolchain_x86_64_unknown_linux_musl, "1.1.0", runtime: false},
+      {:nerves, "~> 1.4", runtime: false},
+      {:nerves_system_br, "1.8.0", runtime: false},
+      {:nerves_toolchain_x86_64_unknown_linux_musl, "1.2.0", runtime: false},
       {:nerves_system_linter, "~> 0.3.0", runtime: false},
       {:ex_doc, "~> 0.18", only: [:dev, :test], runtime: false}
     ]
