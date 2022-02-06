34e291f0485c133d023702f4a7116be112075014
Frank Hunleth <fhunleth@troodon-software.com>
2022-02-06 16:19:30 -0500

    Pass compilation options via TARGET_GCC_FLAGS
    This adds x86_64-specific build options for compiling ports and NIFs.
    Then makes it possible for the NIF/port build options to be the same as
    those used for other C/C++ code in the system (including the Erlang
    runtime).
    
    This also forces `:nerves` `v1.7.15` if using `v1.7.x`. Since
    `TARGET_GCC_FLAGS` was released in a patch release (it's technically
    experimental for now), it's necessary to use `v1.7.15` for this to work.

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule NervesSystemX8664.MixProject do
       env: [
         {"TARGET_ARCH", "x86_64"},
         {"TARGET_OS", "linux"},
-        {"TARGET_ABI", "musl"}
+        {"TARGET_ABI", "musl"},
+        {"TARGET_GCC_FLAGS", "-m64 -fstack-protector-strong -march=nocona -fPIE -pie -Wl,-z,now -Wl,-z,relro"}
       ],
       checksum: package_files()
     ]
@@ -63,7 +64,7 @@ defmodule NervesSystemX8664.MixProject do
 
   defp deps do
     [
-      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.4", runtime: false},
+      {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.15", runtime: false},
       {:nerves_system_br, "1.18.4", runtime: false},
       {:nerves_toolchain_x86_64_nerves_linux_musl, "~> 1.5.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
