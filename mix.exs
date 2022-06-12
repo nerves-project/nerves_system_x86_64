027425d3a5a20f562fd2f6b23fad36141076f9d3
Frank Hunleth <fhunleth@troodon-software.com>
2022-06-11 20:41:52 -0400

    nerves_system_br: bump to v1.20.0

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule NervesSystemX8664.MixProject do
   defp deps do
     [
       {:nerves, "~> 1.5.4 or ~> 1.6.0 or ~> 1.7.15", runtime: false},
-      {:nerves_system_br, "1.19.0", runtime: false},
+      {:nerves_system_br, "1.20.0", runtime: false},
       {:nerves_toolchain_x86_64_nerves_linux_musl, "~> 1.5.0", runtime: false},
       {:nerves_system_linter, "~> 0.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false}
