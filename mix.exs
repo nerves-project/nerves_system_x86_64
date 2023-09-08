46d4c579ef3573b415c47c907a023c39ca835a72
Jon Carstens <jjcarstens@me.com>
2023-09-08 13:38:28 -0600

    Upgrade `fwup-revert.conf` to more capable `fwup-ops.conf`

--- mix.exs
+++ mix.exs
@@ -102,7 +102,7 @@ defmodule NervesSystemX8664.MixProject do
       "fwup_include",
       "rootfs_overlay",
       "CHANGELOG.md",
-      "fwup-revert.conf",
+      "fwup-ops.conf",
       "fwup.conf",
       "grub.cfg",
       "LICENSE",
