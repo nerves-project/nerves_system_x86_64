a4c08b66ae41b69c25a428f1b61d23452aae85a4
Frank Hunleth <fhunleth@troodon-software.com>
2025-10-23 12:47:22 -0400

    Fix hex package file list

--- mix.exs
+++ mix.exs
@@ -109,7 +109,7 @@ defmodule NervesSystemX8664.MixProject do
       "fwup.conf",
       "grub.cfg",
       "LICENSES/*",
-      "linux-6.6.defconfig",
+      "linux-6.12.defconfig",
       "mix.exs",
       "nerves_defconfig",
       "post-build.sh",
